ba1580f7310cedc8a505af472f4ab8d758d7e313
MrBreb <eriksundstrom682@gmail.com>
2026-03-20 14:24:12 +0100

    Bytte färger, Ändrade nav:en helt, började på footern och fixade lite skuggor

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 const nav_button = document.querySelector("#nav_button");
+const nav_menu = document.querySelector("#nav_content");
 
 nav_button.addEventListener('click', () => {
     const currentPos = getComputedStyle(document.documentElement).getPropertyValue('--nav_menu_pos').trim(); /*Denna koden fick jag från Gemini*/
 
     if (currentPos === '0vh'){
-        document.documentElement.style.setProperty('--nav_menu_pos', '-60vh');
+        document.documentElement.style.setProperty('--nav_menu_pos', '-58vh');
     } else {
         document.documentElement.style.setProperty('--nav_menu_pos', '0vh');
     }
