30ba48fed5b5660ceb4bdf06dfd6bfe9df5e0409
MrBreb <eriksundstrom682@gmail.com>
2026-04-17 14:19:15 +0200

    Fixade svart bakgrund när naven är nerfälld

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 const nav_button = document.querySelector("#nav_button");
 const nav_menu = document.querySelector("#nav_content");
+const drop_shadow = document.querySelector("#shadow_behind_nav");
 
 nav_button.addEventListener('click', () => {
-    const currentPos = getComputedStyle(document.documentElement).getPropertyValue('--nav_menu_pos').trim(); /*Denna koden fick jag från Gemini*/
+    const currentPos = getComputedStyle(document.documentElement).getPropertyValue('--nav_menu_pos').trim(); /*Denna koden fick jag från Gemini, men resten av koden skrev jag själv*/
 
     if (currentPos === '0vh'){
-        document.documentElement.style.setProperty('--nav_menu_pos', '-75vh');
+        document.documentElement.style.setProperty('--nav_menu_pos', '-76vh');
+        document.documentElement.style.setProperty('--see_through_val', '0');
     } else {
         document.documentElement.style.setProperty('--nav_menu_pos', '0vh');
+        document.documentElement.style.setProperty('--see_through_val', '0.5');
     }
 });
+
+drop_shadow.addEventListener('click', () => {
+    document.documentElement.style.setProperty('--nav_menu_pos', '-66vh');
+    document.documentElement.style.setProperty('--see_through_val', '0');
+})
