fa247ff9b55309636739fbb3afbd755a4ec99300
MrBreb <eriksundstrom682@gmail.com>
2026-04-17 13:10:06 +0200

    Nu har jag gjort klart hala nav:en. Alltså lagt in bilder och länkar som kommer leda till de andra sidorna :)

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ nav_button.addEventListener('click', () => {
     const currentPos = getComputedStyle(document.documentElement).getPropertyValue('--nav_menu_pos').trim(); /*Denna koden fick jag från Gemini*/
 
     if (currentPos === '0vh'){
-        document.documentElement.style.setProperty('--nav_menu_pos', '-58vh');
+        document.documentElement.style.setProperty('--nav_menu_pos', '-75vh');
     } else {
         document.documentElement.style.setProperty('--nav_menu_pos', '0vh');
     }
